c2eee7c9bb209389b6a24efd20498e70b0beeb3d
rasheedaldeb <rasheedaldeb@gmail.com>
2025-01-08 16:57:38 +0300

    ...

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { HelmetProvider } from "react-helmet-async";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/OFT">
+    <BrowserRouter>
       <HelmetProvider>
         <App />
       </HelmetProvider>
